05cb14948b3c5fec331942b2590fd512a372cbdd
Student <thesunflower23@gmail.com>
2026-02-13 14:20:23 +0800

    Add config.php

--- config.php
+++ config.php
@@ -9,10 +9,10 @@ $db_user = 'root';
 $db_pass = '';
 
 // Uncomment and modify these for InfinityFree hosting:
-// $db_host = 'sql104.infinityfree.com'; // Your InfinityFree MySQL hostname
-// $db_name = 'if0_41145541_login'; // Your database name
-// $db_user = 'if0_41145541'; // Your database username
-// $db_pass = 'keityDa7HSj'; // Your database password
+$db_host = 'sql311.infinityfree.com'; // Your InfinityFree MySQL hostname
+$db_name = 'if0_41146695_login'; // Your database name
+$db_user = 'if0_41146695'; // Your database username
+$db_pass = 'yZ0x8eWKg7goOk'; // Your database password
 
 // Create connection
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
